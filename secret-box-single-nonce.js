c81df51d457ad01c5ab9e4421ffe3a4d413cc645
ameba23 <ameba23@systemli.org>
2020-03-07 19:25:38 +0100

    try secret box with a nonce for the whole stream, for comparison

---
+++ secret-box-single-nonce.js
@@ -0,0 +1,75 @@
+const sodium = require('sodium-native')
+const assert = require('assert')
+const zero = sodium.sodium_memzero
+
+module.exports = encoder
+module.exports.encryptionKey = encryptionKey
+module.exports.KEYBYTES = sodium.crypto_secretbox_KEYBYTES
+
+function encoder (encryptionKey, opts = {}) {
+  assert(Buffer.isBuffer(encryptionKey), 'encryption key must be a buffer')
+  assert(encryptionKey.length === sodium.crypto_secretbox_KEYBYTES, `cobox-crypto: key must be a buffer of length ${sodium.crypto_secretbox_KEYBYTES}`)
+
+  opts.valueEncoding = _resolveStringEncoder(opts.valueEncoding)
+  const nonce = opts.nonce || generateNonce()
+
+  return {
+    encode (message, buffer, offset) {
+      // Run originally provided encoder if any
+      if (opts.valueEncoding && typeof opts.valueEncoding.encode === 'function') {
+        message = opts.valueEncoding.encode(message, buffer, offset)
+      }
+      if (!Buffer.isBuffer(message)) message = Buffer.from(message, 'utf-8')
+      const ciphertext = Buffer.alloc(message.length + sodium.crypto_secretbox_MACBYTES)
+      sodium.crypto_secretbox_easy(ciphertext, message, nonce, encryptionKey)
+      zero(message)
+      return Buffer.concat([nonce, ciphertext])
+    },
+
+    decode (buffer, start, end) {
+      const nonce = buffer.slice(0, sodium.crypto_secretbox_NONCEBYTES)
+      const messageWithMAC = buffer.slice(sodium.crypto_secretbox_NONCEBYTES)
+      const message = Buffer.alloc(messageWithMAC.length - sodium.crypto_secretbox_MACBYTES)
+      assert(
+        sodium.crypto_secretbox_open_easy(message, messageWithMAC, nonce, encryptionKey),
+        'Decryption failed!'
+      )
+      // Run originally provided encoder if any
+      if (opts.valueEncoding && typeof opts.valueEncoding.decode === 'function') {
+        return opts.valueEncoding.decode(message, start, end)
+      } else {
+        return message
+      }
+    },
+    nonce
+  }
+}
+
+function encryptionKey () {
+  const key = sodium.sodium_malloc(sodium.crypto_secretbox_KEYBYTES)
+  sodium.randombytes_buf(key)
+  return key
+}
+
+function generateNonce () {
+  const nonce = Buffer.alloc(sodium.crypto_secretbox_NONCEBYTES)
+  sodium.randombytes_buf(nonce)
+  return nonce
+}
+
+function _resolveStringEncoder (encoder) {
+  if (encoder === 'json') {
+    return {
+      encode: (msg) => Buffer.from(JSON.stringify(msg)),
+      decode: (msg) => JSON.parse(msg.toString())
+    }
+  }
+
+  if (encoder === 'utf-8') {
+    return {
+      encode: (msg) => Buffer.from(msg),
+      decode: (msg) => msg.toString()
+    }
+  }
+  return encoder
+}
