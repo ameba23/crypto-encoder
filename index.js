f64a5d1eb51aaff3a6dc289feca5041321887933
ameba23 <ameba23@systemli.org>
2020-03-07 15:58:49 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 module.exports = {
   secretBox: require('./secret-box'),
-  streamChacha20XOR: require('./cha20stream')
+  streamChacha20XOR: require('./cha20stream'),
+  streamChacha20XORInstance: require('./cha20stream-instance'),
+  streamXOR: require('./XOR-stream')
 }
